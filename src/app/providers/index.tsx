3903900831cad485cecd3f6f0df6727e31d8a499
silchenkoofficial <silchenkoofficial>
2025-03-21 15:31:12 +0300

    feat: adding React-Query & React-Query-Devtools

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -2,13 +2,14 @@
 
 import StyledComponentsRegistry from '@/shared/lib/registry';
 import { ThemeProvider } from '@/shared/theme/provider';
+import { QueryClientProvider } from './query-client-provider';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <QueryClientProvider>
       <StyledComponentsRegistry>
         <ThemeProvider>{children}</ThemeProvider>
       </StyledComponentsRegistry>
-    </>
+    </QueryClientProvider>
   );
 };
